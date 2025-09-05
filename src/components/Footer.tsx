79e3509c800392bc159f081248c914ee61c75e0d
sorokadev <romansoroka.dev@gmail.com>
2025-09-05 10:15:15 +0300

    Enhance Footer component with new button styles and layout; add gradient backgrounds to buttons and update button variants for improved UI consistency.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
+import { Button } from "./ui/button";
+
 export default function Footer() {
   return (
     <footer>
-      <div>Footer</div>
+      <div className="h-[500px] flex w-full flex-col px-4 md:px-[121px]">
+        <div className="flex gap-2.5">
+          <Button size={"lg"}>Test</Button>
+          <Button disabled={true} size={"lg"}>
+            Test
+          </Button>
+        </div>
+        <div className="flex gap-2.5">
+          <Button variant={"secondary"}>Test</Button>
+          <Button variant={"secondary"} disabled={true}>
+            Test
+          </Button>
+        </div>
+      </div>
     </footer>
   );
 }
